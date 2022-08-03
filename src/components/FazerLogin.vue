df8a8e5af4aefff18ea5a1f62e5fca2941d55337
Umdyne <luansilva2357@gmail.com>
2022-08-02 23:57:10 -0300

    HeadSemiPronto

---
+++ src/components/FazerLogin.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex border border-azulYT p-3 h-9 mr-8 items-center ">
+        <img src="./icones/avataryoutube.png" class="mr-2">
+        <a href="#" class="text-azulYT font-semibold text-sm">FAZER LOGIN</a>
+        
+    </div>
+</template>
+
--- src/components/PaginaInicial.vue
+++ src/components/PaginaInicial.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="bg-cinzaYT1 h-full w-full text-white  ">
-    <header class="flex pt-4 h-10 w-full items-center"> 
-      head
+    <header class="flex pt-4 h-10 w-full items-center  justify-between">
+      <div class="flex"> 
+        <MenuIcon class="ml-6 mt-1"/>
+       <img src="./images/logo-youtube.png" class="ml-5 mb-1">  
+      </div> 
+      <div> 
+        <InputSearch class=""/>  
+      </div>
+      <div class="flex"> 
+        <DotsIcon class="w-5 mr-4"/>
+        <FazerLogin />
+      </div>
+      
+
     </header>
-    <hr class="w-full p-4 border-0.1 opacity-10">
+   
     
-    <body class="">
+    <body class="mt-4 bg-cinzaYT2 ">
+       <hr class="w-full mw-6  border-0.1 opacity-10">
       body
     </body>
   </div>
@@ -13,6 +26,12 @@
 </template>
 
 <script lang="ts" setup>
+import DotsIcon from './inputs/DotsIcon.vue';
+import MenuIcon from './inputs/MenuIcon.vue';
+import InputSearch from './inputs/InputSearch.vue';
+import DotsIcon1 from './inputs/DotsIcon.vue';
+import FazerLogin from './FazerLogin.vue';
+
 
 
 </script>
